721a58fff92bd945a5b88a500349bd46041520a0
Aarti Kumari <kaarti.kc@gmail.com>
2025-05-28 16:50:37 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  server: {
+    host: true, // This makes it use 0.0.0.0 instead of localhost
+    port: process.env.PORT || 5173,
+  },
+};
+
